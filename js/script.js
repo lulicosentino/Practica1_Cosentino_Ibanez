9d58fa2f8561854e7a5036645c97779ef8a3d1f8
beaibanez02 <bea.ibanez02@gmail.com>
2023-11-15 15:41:23 +0100

    Ejemplo change src

--- js/script.js
+++ js/script.js
@@ -5,6 +5,23 @@ $(document).ready(function(){
         $(".navbar-menu").toggleClass("active");
     });
 
+    function changeImage() {
+        "use strict";
+        console.log("hovering in n out");
+    
+        var oldSrc = document.getElementById("letsee").getAttribute("src");
+    
+    
+        if (oldSrc === "imgs/entradas-20.svg"){
+            document.getElementById("letsee").setAttribute("src", "imgs/flor_button.svg");
+        }
+        else{
+            document.getElementById("letsee").setAttribute("src", "imgs/entradas-20.svg");
+            console.log("¡¡vuelvo a como era antes!!");
+        }
+    
+    
+    }
     
 
 
