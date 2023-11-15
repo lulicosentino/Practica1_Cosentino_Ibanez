c2db99640472d0c7add84a93ee298db428abde81
beaibanez02 <bea.ibanez02@gmail.com>
2023-11-16 00:23:39 +0100

    modal

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
-$(document).ready(function(){
+// $(document).ready(function(){
     
-    $(".menu").click(function() {
-        $(".menu").toggleClass("active");
-        $(".navbar-menu").toggleClass("active");
-    });
+//     $(".menu").click(function() {
+//         $(".menu").toggleClass("active");
+//         $(".navbar-menu").toggleClass("active");
+//     });
 
-    function changeImage() {
-        "use strict";
-        console.log("hovering in n out");
-    
-        var oldSrc = document.getElementById("letsee").getAttribute("src");
-    
-    
-        if (oldSrc === "imgs/entradas-20.svg"){
-            document.getElementById("letsee").setAttribute("src", "imgs/flor_button.svg");
-        }
-        else{
-            document.getElementById("letsee").setAttribute("src", "imgs/entradas-20.svg");
-            console.log("¡¡vuelvo a como era antes!!");
-        }
-    
-    
-    }
-    
 
 
 
     
-});
+// });
+function cerrar_modal(){
+    "use strict";
+    document.getElementById("modal_container").style.display = "none";
+}
+
+function ventana_modal(){
+    document.getElementById("modal_container").style.display = "flex";
+}
+
+
+
+// function cerrarVentana() {
+//     "use strict";
+//     document.getElementById("modal").style.display = "none";
+// }
