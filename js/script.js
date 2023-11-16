021867e4332b75871c4715a32565378d58417ef2
beaibanez02 <bea.ibanez02@gmail.com>
2023-11-16 01:55:11 +0100

    cambios product.html, css &co

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
-// $(document).ready(function(){
+$(document).ready(function(){
     
-//     $(".menu").click(function() {
-//         $(".menu").toggleClass("active");
-//         $(".navbar-menu").toggleClass("active");
-//     });
+    // $(".menu").click(function() {
+    //     $(".menu").toggleClass("active");
+    //     $(".navbar-menu").toggleClass("active");
+    // });
 
+    $("#likerred").click(function (){
+        console.log("click funciona");
+
+        $("#likerred").attr("src", "media/galeria/icons/likedheart.svg");
+        
+
+    });
+
+    $("#carro_modal").click(function (){
+        console.log("click fucniona");
+
+        $("#modal_container").attr("display", "flex");
+
+
+    });
 
 
 
     
-// });
-function cerrar_modal(){
-    "use strict";
-    document.getElementById("modal_container").style.display = "none";
-}
+});
 
-function ventana_modal(){
-    document.getElementById("modal_container").style.display = "flex";
-}
+
+
+// function cerrar_modal(){
+//     "use strict";
+//     document.getElementById("modal_container").style.display = "none";
+// }
+
+// function ventana_modal(){
+//     document.getElementById("modal_container").style.display = "flex";
+// }
 
 
 
